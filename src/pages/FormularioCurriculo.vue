b1ed97e3393570ab84f0a0042f5580e44a87df2b
Gustavo Rizzo Albuquerque <grizzo.albu@gmail.com>
2020-05-12 14:03:07 -0300

    começando a construir o FormularioCurriculo

---
+++ src/pages/FormularioCurriculo.vue
@@ -0,0 +1,72 @@
+<template>
+<div>
+    <form class="form">
+
+        <div class="dados-pessois">
+
+            <div class="form-group">
+                <label for="nome">Nome Completo</label>
+                <input type="text" id="nome" class="form-control" required autofocus />
+            </div>
+
+            <div class="form-group">
+                <label for="profissao">Profissão</label>
+                <input type="text" id="profissao" class="form-control" required />
+            </div>
+
+            <div class="form-group">
+                <label for="naturalidade">Naturalidade</label>
+                <input type="text" id="naturalidade" class="form-control" required />
+            </div>
+
+            <div class="form-group">
+                <label for="dataNascimento">Data de Nascimento</label>
+                <input type="date" id="dataNascimento" class="form-control" required/>
+            </div>
+
+            <div class="form-group">
+                <label for="ddd">DDD</label>
+                <input type="number" id="ddd" class="form-control" required />
+            </div>
+
+            <div class="form-group">
+                <label for="celular">Celular</label>
+                <input type="tel" id="celular" class="form-control" required />
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" class="form-control" required />
+            </div>
+        </div>
+
+        <div class="form-endereco">
+            <div class="form-group">
+                <label for="pais">Pais</label>
+                <input type="text" id="pais" class="form-control" required />
+            </div>
+
+            <div class="form-group">
+                <label for="uf">UF</label>
+                <input type="text" id="uf" class="form-control" required />
+            </div>
+
+            <div class="form-group">
+                <label for="cidade">Cidade</label>
+                <input type="text" id="cidade" class="form-control" required />
+            </div>
+        </div>
+
+        <button class="btn btn-primary" type="submit">Incluir</button>
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'FormularioCurriculo',
+    data() {
+        return {}
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Contratado from '../pages/Contratado'
+import FormularioCurriculo from '../pages/FormularioCurriculo'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Contratado',
       component: Contratado
+    },
+    {
+      path: '/FormularioCurriculo',
+      name: 'FormularioCurriculo',
+      component: FormularioCurriculo
     }
   ]
 })
