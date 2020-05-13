b673933fd951bf415d54acb8d932946ed0a8eee6
Gustavo Rizzo Albuquerque <grizzo.albu@gmail.com>
2020-05-13 19:48:20 -0300

    FormularioCurriculo - corrigindo data formulário

--- src/pages/FormularioCurriculo.vue
+++ src/pages/FormularioCurriculo.vue
@@ -6,60 +6,61 @@
 
             <div class="form-group">
                 <label for="nome">Nome Completo</label>
-                <input type="text" id="nome" class="form-control" required autofocus v-model=infoPessoais.nome />
+                <input type="text" id="nome" class="form-control" required autofocus v-model="infoPessoais.nome" />
             </div>
 
             <div class="form-group">
                 <label for="profissao">Profissão</label>
-                <input type="text" id="profissao" class="form-control" required  v-model=infoPessoais.profissao />
+                <input type="text" id="profissao" class="form-control" required  v-model="infoPessoais.profissao" />
             </div>
 
             <div class="form-group">
                 <label for="naturalidade">Naturalidade</label>
-                <input type="text" id="naturalidade" class="form-control" required v-model=infoPessoais.naturalidade />
+                <input type="text" id="naturalidade" class="form-control" required v-model="infoPessoais.naturalidade" />
             </div>
 
             <div class="form-group">
                 <label for="dataNascimento">Data de Nascimento</label>
-                <input type="date" id="dataNascimento" class="form-control" required v-model=infoPessoais.dataNascimento />
+                <input type="date" id="dataNascimento" class="form-control" required v-on:input="infoPessoais.dataNascimento = new Date($event.target.value)"/>
             </div>
 
             <div class="form-group">
                 <label for="estadoCivil">Estado Civil</label>
-                <input type="text" id="estadoCivil" class="form-control" required v-model=infoPessoais.estadoCivil />
+                <input type="text" id="estadoCivil" class="form-control" required v-model="infoPessoais.estadoCivil" />
             </div>
 
             <div class="form-group">
                 <label for="ddd">DDD</label>
-                <input type="number" id="ddd" class="form-control" required v-model=infoPessoais.ddd />
+                <input type="number" id="ddd" class="form-control" required v-model="infoPessoais.ddd" />
             </div>
 
             <div class="form-group">
                 <label for="celular">Celular</label>
-                <input type="tel" id="celular" class="form-control" required v-model=infoPessoais.celular />
+                <input type="tel" id="celular" class="form-control" required v-model="infoPessoais.celular" />
             </div>
 
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" id="email" class="form-control" required v-model=infoPessoais.email />
+                <input type="email" id="email" class="form-control" required v-model="infoPessoais.email" />
             </div>
         </div>
 
         <div class="form-endereco">
             <div class="form-group">
                 <label for="pais">Pais</label>
-                <input type="text" id="pais" class="form-control" required v-model=endereco.pais />
+                <input type="text" id="pais" class="form-control" required v-model="endereco.pais" />
             </div>
 
             <div class="form-group">
                 <label for="uf">UF</label>
-                <input type="text" id="uf" class="form-control" required v-model=endereco.uf />
+                <input type="text" id="uf" class="form-control" required v-model="endereco.uf" />
             </div>
 
             <div class="form-group">
                 <label for="cidade">Cidade</label>
-                <input type="text" id="cidade" class="form-control" required v-model=endereco.cidade />
+                <input type="text" id="cidade" class="form-control" required v-model="endereco.cidade" />
             </div>
+
         </div>
 
         <button class="btn btn-primary" type="submit">Incluir</button>
@@ -76,7 +77,8 @@ export default {
     data() {
         return {
             endereco: new Endereco(),
-            infoPessoais: new InformacoesPessoais()
+            infoPessoais: new InformacoesPessoais(),
+            data_teste: new Date()
         }
     },
     methods: {
